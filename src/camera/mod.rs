671fabbd101619046b3ddad22cc9539102303bb3
Wesley Feldsine <laocoon7@gmail.com>
2023-03-14 15:12:10 -0700

    add tracking camera

--- src/camera/mod.rs
+++ src/camera/mod.rs
@@ -3,6 +3,8 @@ use bevy::{
     prelude::*,
 };
 
+use crate::{player::PlayerTag, states::AppState};
+
 #[derive(Component)]
 pub struct GameCameraTag;
 
@@ -11,6 +13,7 @@ impl Plugin for CameraPlugin {
     fn build(&self, app: &mut App) {
         trace!("Adding CameraPlugin systems");
         app.add_startup_system(init_game_camera);
+        app.add_system(center_camera_on_player.in_set(OnUpdate(AppState::Game)));
     }
 }
 
@@ -28,4 +31,19 @@ fn init_game_camera(mut commands: Commands) {
         BloomSettings::default(),
         GameCameraTag,
     ));
+}
+
+fn center_camera_on_player(
+    mut camera_query: Query<&mut Transform, (With<GameCameraTag>, Without<PlayerTag>)>,
+    player_query: Query<&Transform, (With<PlayerTag>, Without<GameCameraTag>)>,
+) {
+    for player_transform in player_query.iter() {
+        for mut camera_transform in camera_query.iter_mut() {
+            camera_transform.translation = Vec3 {
+                x: player_transform.translation.x,
+                y: player_transform.translation.y,
+                z: camera_transform.translation.z,
+            };
+        }
+    }
 }
